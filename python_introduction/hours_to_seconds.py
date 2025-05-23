08af521ebccfeeb041b099aad68b0d69e7dc8cd9
Iroka chinedu <irokachinedu7@gmail.com>
2025-05-23 15:00:09 +0100

    edited script

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,5 +1,6 @@
-seconds = 3600
-hours = seconds * 2
-print("2 hour(s) is", hours, "seconds.")
+seconds_per_hour = 3600
+hours = 2
+seconds = seconds_per_hour * hours
+print("2 hour(s) is", seconds, "seconds.")
 
 
