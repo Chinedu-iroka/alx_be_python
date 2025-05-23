e65bfd4d0d5b57f47c607df0cd3af7356a9de1e4
Iroka chinedu <irokachinedu7@gmail.com>
2025-05-23 11:30:18 +0100

    initial commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+sum_result = number1 + number2
+substraction_result = number1 - number2
+multiplication_result = number1 * number2
+print("Addition of", number1, "and", number2, "is", sum_result)
+print("Substraction of", number1, "and", number2, "is", substraction_result)
+print("Multiplication of", number1, "and", number2, "is", multiplication_result)
+
