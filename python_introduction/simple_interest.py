bb2784c502e5f611390e1d91cf6ee143c7f59511
Iroka chinedu <irokachinedu7@gmail.com>
2025-05-23 14:25:13 +0100

    calculation now on one line

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,8 +1,6 @@
-principle = 1000
-rate = 0.05
-time = 3
+principal = 1000, rate = 0.5, time = 3
 
-interest = principle * rate * time
+interest = principal * rate * time
 
 print("The simple interest is:", interest)
 
