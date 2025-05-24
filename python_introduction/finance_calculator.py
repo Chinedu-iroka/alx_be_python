4a46c631a4ebbe20efe6357aeb89beef1333dc31
chinedu <irokachinedu7@gmail.com>
2025-05-24 11:18:56 +0100

    added a monthly salary calculator

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,12 @@
+monthly_income = input("Enter your monthly income:")
+monthly_expenses = input("Enter your total monthly expenses:")
+m1 = int(monthly_income)
+m2 = int(monthly_expenses)
+monthly_savings = m1 - m2
+#simple intrerest is  (i = p * r * t)
+# principle =monthly_savings
+# rate = o.05
+# time = 12
+interest = monthly_savings * 0.05 * 12
+print("your monthly savings are", monthly_savings)
+print("projected savings after one year, with interest, is:", interest) 
