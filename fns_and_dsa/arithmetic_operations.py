591aa477833a44a7f15cac3018bb5dd6424c6f73
chinedu <irokachinedu7@gmail.com>
2025-06-09 19:25:36 +0100

    still editing the fomular

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,13 @@
-def perform_operation(num1: float, num2: float, operation: str):
-    if operation == 'addition':
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
         return num1 + num2
-    elif operation == 'substraction':
+    elif operation == 'subtract':
         return num1 - num2
-    elif operation == 'multiplication':
+    elif operation == 'multiply':
         return num1 * num2
-    elif operation == 'division':
-        if num2 !=0:
-            return num1 / num2
-        else:
-            return "Error: Division by zero."
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
     else:
-        return "Error: Unsupported operation."
+        return "Error: Unsupported operation"
