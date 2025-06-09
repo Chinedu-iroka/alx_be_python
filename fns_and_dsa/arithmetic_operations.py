975d19462256e50a4e64a4b74bc80290e24f9a8a
chinedu <irokachinedu7@gmail.com>
2025-06-09 19:21:15 +0100

    edited fomular

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,17 +1,17 @@
 def perform_operation():
-    number1 = float
-    number2 = float
+    num1 = float
+    num2 = float
     operation = str
 
     if operation == 'addition':
-        return number1 + number2
+        return num1 + num2
     elif operation == 'substraction':
-        return number1 - number2
+        return num1 - num2
     elif operation == 'multiplication':
-        return number1 * number2
+        return num1 * num2
     elif operation == 'division':
-        if number2 !=0:
-            return number1 / number2
+        if num2 !=0:
+            return num1 / num2
         else:
             return "Error: Division by zero."
     else:
